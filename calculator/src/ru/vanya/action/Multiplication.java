e4ebb6ef6b943059cdf5c177d0c751d1a2243279
ivankuk <iv.kuklick2016@ya.ru>
2017-08-05 01:03:28 +0300

    Это пока скелет класса умножения

---
+++ calculator/src/ru/vanya/action/Multiplication.java
@@ -0,0 +1,16 @@
+package ru.vanya.action;
+import java.util.Scanner;
+
+public class Multiplication {
+
+  private final Scanner SCANNER = new Scanner(System.in);
+
+  private final String MULTIPLICATION_SIGN = " X ";
+
+  private final String SAME_SIGN = " = ";
+
+  private final int INTEGER_NUMBER = 1;
+
+  private final int FRACTINAL_NUMBER = 2;
+
+}
