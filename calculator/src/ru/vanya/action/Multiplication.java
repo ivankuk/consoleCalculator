26b458ab691e3bba047a8694c9156952bb569804
ivankuk <iv.kuklick2016@ya.ru>
2017-08-06 22:58:58 +0300

    Готов алгоритм для умножения целых чисел

--- calculator/src/ru/vanya/action/Multiplication.java
+++ calculator/src/ru/vanya/action/Multiplication.java
@@ -15,6 +15,50 @@ public class Multiplication {
 
   public int showResult() {
 
-    
+    System.out.println();
+
+     System.out.println(INTEGER_NUMBER + " - Целое число");
+
+     System.out.println(FRACTINAL_NUMBER + " - Дробное число");
+
+     int choice = SCANNER.nextInt();
+
+     switch (choice) {
+
+        case INTEGER_NUMBER:
+
+        System.out.println();
+
+        System.out.println("Введите первый множитель ");
+
+        System.out.println();
+
+        System.out.println("> ");
+
+        int theFirstMultiplier = SCANNER.nextInt();
+
+        System.out.println();
+
+        System.out.println("Введите второй множитель ");
+
+        System.out.println();
+
+        System.out.println("> ");
+
+        int theSecondMultiplier = SCANNER.nextInt();
+
+        System.out.println();
+
+        int theComposite = theFirstMultiplier * theSecondMultiplier;
+
+        System.out.println(theFirstMultiplier + MULTIPLICATION_SIGN + theSecondMultiplier + theComposite);
+
+        break;
+        
+     }
+
+     return INTEGER_NUMBER;
+
   }
+
 }
