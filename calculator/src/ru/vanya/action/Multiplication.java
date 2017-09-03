cfd8bb340988354a3e9c2835211e8ca4f2863284
ivankuk <iv.kuklick2016@ya.ru>
2017-09-03 23:03:41 +0300

    Готов алгоритм для умножения дробных чисел

--- calculator/src/ru/vanya/action/Multiplication.java
+++ calculator/src/ru/vanya/action/Multiplication.java
@@ -51,10 +51,42 @@ public class Multiplication {
 
         int theComposite = theFirstMultiplier * theSecondMultiplier;
 
-        System.out.println(theFirstMultiplier + MULTIPLICATION_SIGN + theSecondMultiplier + theComposite);
+        System.out.println(theFirstMultiplier + MULTIPLICATION_SIGN + theSecondMultiplier + SAME_SIGN + theComposite);
 
         break;
-        
+
+        case FRACTINAL_NUMBER:
+
+        System.out.println();
+
+        System.out.println("Ввод дробей такой 1,0");
+
+        System.out.println();
+
+        System.out.println("Введите первый множитель ");
+
+        System.out.println();
+
+        System.out.println("> ");
+
+        float firstMultiplier = SCANNER.nextFloat();
+
+        System.out.println();
+
+        System.out.println("Введите второй множитель ");
+
+        System.out.println();
+
+        System.out.println("> ");
+
+        float secondMultiplier = SCANNER.nextFloat();
+
+        System.out.println();
+
+        float composite = firstMultiplier * secondMultiplier;
+
+        System.out.println(firstMultiplier + MULTIPLICATION_SIGN + secondMultiplier + SAME_SIGN + composite);
+
      }
 
      return INTEGER_NUMBER;
