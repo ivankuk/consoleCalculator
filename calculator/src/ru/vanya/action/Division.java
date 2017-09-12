f3ba69d0c02b9be6ae65b245f3b122031be8b229
ivankuk <iv.kuklick2016@ya.ru>
2017-09-12 17:47:52 +0300

    Это пока скелет класса деление

---
+++ calculator/src/ru/vanya/action/Division.java
@@ -0,0 +1,20 @@
+package ru.vanya.action;
+import java.util.Scanner;
+
+public class Division {
+
+  private final Scanner SCANNER = new Scanner(System.in);
+
+  private final String DIVISION_SING = " : ";
+
+  private final String SAME_SIGN = " = ";
+
+  private final int INTEGER_NUMBER = 1;
+
+  private final int FRACTINAL_NUMBER = 2;
+
+  private float translationTypeData() {}
+
+  public int showResult() {}
+
+}
